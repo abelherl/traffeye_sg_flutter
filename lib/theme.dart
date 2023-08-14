3f4babad0d419777952afeb57b48ab8ae668e1e6
abelherl <abangabel02@gmail.com>
2023-08-14 14:09:46 +0700

    [CHG] Formatting updates for theme.dart

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class TraffEyeAppTheme {
-  TraffEyeAppTheme._();
-
-  // *****************
-  // static colors
-  // *****************
+abstract class TraffEyeAppTheme {
+  // * Static Colors - Light
   static const Color _lightPrimaryColor = Color(0xFF143A62);
   static const Color _lightPrimaryVariantColor = Color(0xFF0F2A48);
   static const Color _lightOnPrimaryColor = Color(0xFFF8F8F8);
@@ -16,19 +12,14 @@ class TraffEyeAppTheme {
   static const Color _lightAccentColor = Color(0xFFFFC08A);
   static const Color _lightAccentVariantColor = Color(0xFFFF9840);
   static const Color _lightBackgroundColor = Color(0xFFF8F8F8);
-
   static const Color _lightCaptionColor = Color(0xFF7D7D7D);
   static const Color _lightDisableColor = Color(0xFFB6B6B6);
   static const Color _lightHintColor = Color(0xFF9E9E9E);
-
   static const Color _lightPositiveColor = Color(0xFF6DBB61);
   static const Color _lightNegativeColor = Color(0xFFCB4444);
 
+  // * Text Style - Light
   static final TextStyle _baseTextStyle = GoogleFonts.montserrat();
-
-  // *****************
-  // Text Style - light
-  // *****************
 
   static final TextStyle _lightDisplayText = _baseTextStyle.copyWith(
     color: _lightOnBackgroundColor,
@@ -61,10 +52,7 @@ class TraffEyeAppTheme {
     bodySmall: _lightCaptionText,
   );
 
-  // *****************
-  // Theme light/dark
-  // *****************
-
+  // * Theme
   static final ThemeData lightTheme = ThemeData(
     scaffoldBackgroundColor: _lightPrimaryColor,
     appBarTheme: const AppBarTheme(
