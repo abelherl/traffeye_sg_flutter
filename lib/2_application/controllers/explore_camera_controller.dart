91b070b0b3346e9bf0a5d30c413c087be857eef9
abelherl <abangabel02@gmail.com>
2023-09-11 17:02:44 +0700

    [ADD] Added explore camera controller

---
+++ lib/2_application/controllers/explore_camera_controller.dart
@@ -0,0 +1,36 @@
+import 'package:get/get.dart';
+import 'package:traffeye_sg_flutter/1_domain/entities/traffic_camera_entity.dart';
+import 'package:traffeye_sg_flutter/2_application/controllers/camera_controller.dart';
+
+class ExploreCameraController extends GetxController {
+  final initialCameras = <TrafficCameraEntity>[].obs;
+  final searchedCameras = <TrafficCameraEntity>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    final cameraController = Get.find<CameraController>();
+    final cameras = cameraController.cameras;
+    initialCameras.value = cameras;
+    searchedCameras.value = cameras;
+  }
+
+  void searchFor(String keyword) {
+    if (keyword == '') {
+      searchedCameras.value = initialCameras;
+      return;
+    }
+
+    searchedCameras.value = initialCameras.value.where((element) {
+      late final bool isNameFound;
+
+      if (element.customName == null) {
+        isNameFound = element.location.name.contains(keyword);
+      } else {
+        isNameFound = element.customName!.contains(keyword);
+      }
+
+      return isNameFound;
+    }).toList();
+  }
+}
