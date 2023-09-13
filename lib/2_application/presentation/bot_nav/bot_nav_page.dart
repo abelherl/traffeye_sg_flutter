5fe1c6aaf7ac69fb612be1220cf1a0ddf242998d
abelherl <abangabel02@gmail.com>
2023-09-13 14:09:05 +0700

    [ADD] Added Saved Cameras page

--- lib/2_application/presentation/bot_nav/bot_nav_page.dart
+++ lib/2_application/presentation/bot_nav/bot_nav_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:traffeye_sg_flutter/2_application/presentation/bot_nav/widgets/app_bot_nav_bar.dart';
 import 'package:traffeye_sg_flutter/2_application/presentation/bot_nav/widgets/fade_indexed_stack.dart';
 import 'package:traffeye_sg_flutter/2_application/presentation/dashboard/dashboard_page.dart';
+import 'package:traffeye_sg_flutter/2_application/presentation/saved_cameras/saved_cameras_page.dart';
 
 import 'widgets/app_floating_action_button.dart';
 
@@ -16,6 +17,8 @@ class BotNavPage extends StatelessWidget {
         index: 0,
         children: [
           DashboardPage(),
+          SavedCamerasPage(),
+          Placeholder(),
         ],
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
