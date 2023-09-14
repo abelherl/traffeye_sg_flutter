cdc31cffc09e3799343f28f8902f1c06cc1bab03
abelherl <abangabel02@gmail.com>
2023-09-14 14:35:54 +0700

    [ADD|CHG] Extracted widgets

--- lib/2_application/presentation/saved_cameras/saved_cameras_page.dart
+++ lib/2_application/presentation/saved_cameras/saved_cameras_page.dart
@@ -1,10 +1,9 @@
-import 'package:drag_and_drop_lists/drag_and_drop_lists.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:traffeye_sg_flutter/2_application/controllers/saved_cameras_controller.dart';
 import 'package:traffeye_sg_flutter/2_application/core/helpers/intl_helper.dart';
 import 'package:traffeye_sg_flutter/2_application/core/widgets/warning_widget.dart';
-import 'package:traffeye_sg_flutter/2_application/presentation/saved_cameras/widgets/saved_camera_list_item.dart';
+import 'package:traffeye_sg_flutter/2_application/presentation/saved_cameras/widgets/saved_cameras_list.dart';
 
 class SavedCamerasPage extends StatelessWidget {
   const SavedCamerasPage({super.key});
@@ -30,25 +29,7 @@ class SavedCamerasPage extends StatelessWidget {
                   title: IntlHelper.errorNoSavedCamerasTitle.tr,
                   subtitle: IntlHelper.errorNoSavedCamerasSubtitle.tr,
                 )
-              : SingleChildScrollView(
-                  physics: const BouncingScrollPhysics(),
-                  child: DragAndDropLists(
-                    itemDragOnLongPress: false,
-                    disableScrolling: true,
-                    children: [
-                      DragAndDropList(
-                        canDrag: false,
-                        children: controller.savedCameras.map((camera) {
-                          return DragAndDropItem(
-                            child: SavedCameraListItem(camera: camera),
-                          );
-                        }).toList(),
-                      ),
-                    ],
-                    onItemReorder: controller.reorder,
-                    onListReorder: (_, __) {},
-                  ),
-                ),
+              : const SavedCamerasList(),
         ),
       ),
     );
---
+++ lib/2_application/presentation/saved_cameras/widgets/saved_cameras_list.dart
@@ -0,0 +1,33 @@
+import 'package:drag_and_drop_lists/drag_and_drop_lists.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:traffeye_sg_flutter/2_application/controllers/saved_cameras_controller.dart';
+import 'package:traffeye_sg_flutter/2_application/presentation/saved_cameras/widgets/saved_camera_list_item.dart';
+
+class SavedCamerasList extends StatelessWidget {
+  const SavedCamerasList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.find<SavedCamerasController>();
+    return SingleChildScrollView(
+      physics: const BouncingScrollPhysics(),
+      child: DragAndDropLists(
+        itemDragOnLongPress: false,
+        disableScrolling: true,
+        children: [
+          DragAndDropList(
+            canDrag: false,
+            children: controller.savedCameras.map((camera) {
+              return DragAndDropItem(
+                child: SavedCameraListItem(camera: camera),
+              );
+            }).toList(),
+          ),
+        ],
+        onItemReorder: controller.reorder,
+        onListReorder: (_, __) {},
+      ),
+    );
+  }
+}
