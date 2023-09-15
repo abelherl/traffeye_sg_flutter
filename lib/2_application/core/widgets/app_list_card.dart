4abd3a08c646ab2a65a7976428d810bf1df022a7
abelherl <abangabel02@gmail.com>
2023-09-15 10:15:56 +0700

    [ADD] Added list card variations

--- lib/2_application/core/widgets/app_list_card.dart
+++ lib/2_application/core/widgets/app_list_card.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:traffeye_sg_flutter/2_application/core/helpers/assets_path_helper.dart';
 import 'package:traffeye_sg_flutter/2_application/core/helpers/style_helper.dart';
 import 'package:traffeye_sg_flutter/2_application/core/widgets/app_ink_well.dart';
+import 'package:traffeye_sg_flutter/2_application/widgets/themed_text.dart';
+import 'package:traffeye_sg_flutter/theme.dart';
 
 abstract class AppListCard {
-  static Widget normal({required Widget child, required Function() onPressed}) {
+  static Widget normal(
+      {required Widget child,
+      required Function() onPressed}) {
     return _NormalAppListCard(onPressed: onPressed, child: child);
+  }
+
+  static Widget profileBasic(
+      {required String title,
+      required Function() onPressed,
+      String suffix = ''}) {
+    return _ProfileBasicListCard(
+        title: title, onPressed: onPressed, suffix: suffix);
   }
 }
 
@@ -56,6 +70,45 @@ class _NormalAppListCard extends StatelessWidget {
     return _BaseAppListCard(
       onPressed: onPressed,
       child: child,
+    );
+  }
+}
+
+class _ProfileBasicListCard extends StatelessWidget {
+  final String title;
+  final Function() onPressed;
+  final String suffix;
+
+  const _ProfileBasicListCard({
+    required this.title,
+    required this.onPressed,
+    this.suffix = '',
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return _BaseAppListCard(
+      onPressed: onPressed,
+      child: Row(
+        children: [
+          Expanded(
+            child: ThemedText(
+              title,
+              themedTextStyle: ThemedTextStyle.body,
+              maxLines: 2,
+            ),
+          ),
+          SizedBox(width: 10.w),
+          ThemedText(
+            suffix,
+            themedTextStyle: ThemedTextStyle.caption,
+            color: AppTheme.lightCaptionColor,
+            maxLines: 1,
+          ),
+          SizedBox(width: 10.w),
+          SvgPicture.asset(AssetsPathHelper.filledNext),
+        ],
+      ),
     );
   }
 }
