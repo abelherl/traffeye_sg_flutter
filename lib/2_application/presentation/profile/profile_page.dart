1f6db5a25110b84358d6b14c898385d1c4689fcb
abelherl <abangabel02@gmail.com>
2023-09-15 15:49:05 +0700

    [ADD|CHG] Extracted widgets

--- lib/2_application/presentation/profile/profile_page.dart
+++ lib/2_application/presentation/profile/profile_page.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:traffeye_sg_flutter/2_application/controllers/camera_controller.dart';
 import 'package:traffeye_sg_flutter/2_application/core/helpers/intl_helper.dart';
-import 'package:traffeye_sg_flutter/2_application/core/widgets/app_list_card.dart';
 import 'package:traffeye_sg_flutter/2_application/core/widgets/app_scroll_view.dart';
-import 'package:traffeye_sg_flutter/2_application/widgets/themed_text.dart';
+import 'package:traffeye_sg_flutter/2_application/presentation/profile/sections/dashboard_section.dart';
+import 'package:traffeye_sg_flutter/2_application/presentation/profile/sections/other_section.dart';
+import 'package:traffeye_sg_flutter/2_application/presentation/profile/sections/personalization_section.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -22,52 +22,11 @@ class ProfilePage extends StatelessWidget {
         color: Theme.of(context).colorScheme.background,
         child: AppScrollView(
           padding: EdgeInsets.only(bottom: 100.w),
-          child: Column(
+          child: const Column(
             children: [
-              ProfileSection(
-                title: IntlHelper.personalization.tr,
-                appListCards: [
-                  AppListCard.profileBasic(
-                    title: IntlHelper.cardWidgetsTitle.tr,
-                    onPressed: () {},
-                  ),
-                  AppListCard.profileBasic(
-                    title: IntlHelper.dashboardLayout.tr,
-                    suffix: IntlHelper.carousel.tr,
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-              ProfileSection(
-                title: IntlHelper.sections.tr,
-                appListCards: [
-                  AppListCard.profileSwitch(
-                    title: IntlHelper.savedCameras.tr,
-                    value: Get.find<CameraController>().isHideRefreshButton,
-                  ),
-                  AppListCard.profileSwitch(
-                    title: IntlHelper.cardCamerasTitle.tr,
-                    value: Get.find<CameraController>().isHideRefreshButton,
-                  ),
-                  AppListCard.profileSwitch(
-                    title: IntlHelper.homeWidgetsTitle.tr,
-                    value: Get.find<CameraController>().isHideRefreshButton,
-                  ),
-                ],
-              ),
-              ProfileSection(
-                title: IntlHelper.other.tr,
-                appListCards: [
-                  AppListCard.profileBasic(
-                    title: IntlHelper.about.tr,
-                    onPressed: () {},
-                  ),
-                  AppListCard.profileBasic(
-                    title: IntlHelper.donate.tr,
-                    onPressed: () {},
-                  ),
-                ],
-              ),
+              PersonalizationSection(),
+              DashboardSection(),
+              OtherSection(),
             ],
           ),
         ),
@@ -76,31 +35,3 @@ class ProfilePage extends StatelessWidget {
   }
 }
 
-class ProfileSection extends StatelessWidget {
-  final String title;
-  final List<Widget> appListCards;
-
-  const ProfileSection({
-    super.key,
-    required this.title,
-    required this.appListCards,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: EdgeInsets.all(16.w).copyWith(bottom: 0),
-          child: ThemedText(
-            title,
-            themedTextStyle: ThemedTextStyle.title,
-            color: Theme.of(context).colorScheme.primary,
-          ),
-        ),
-        ...appListCards,
-      ],
-    );
-  }
-}
