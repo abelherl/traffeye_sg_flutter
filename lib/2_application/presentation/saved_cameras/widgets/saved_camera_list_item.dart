0c4a09faf3058db2ec8a62c1526b31ed27044e85
abelherl <abangabel02@gmail.com>
2023-09-15 09:41:33 +0700

    [CHG] Extracted app list card

--- lib/2_application/presentation/saved_cameras/widgets/saved_camera_list_item.dart
+++ lib/2_application/presentation/saved_cameras/widgets/saved_camera_list_item.dart
@@ -3,8 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:traffeye_sg_flutter/1_domain/entities/traffic_camera_entity.dart';
 import 'package:traffeye_sg_flutter/2_application/core/helpers/assets_path_helper.dart';
-import 'package:traffeye_sg_flutter/2_application/core/helpers/style_helper.dart';
-import 'package:traffeye_sg_flutter/2_application/core/widgets/app_ink_well.dart';
+import 'package:traffeye_sg_flutter/2_application/core/widgets/app_list_card.dart';
 import 'package:traffeye_sg_flutter/2_application/presentation/camera_details/camera_details_pop_up.dart';
 import 'package:traffeye_sg_flutter/2_application/widgets/themed_text.dart';
 
@@ -15,33 +14,22 @@ class SavedCameraListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(16.w).copyWith(bottom: 0),
-      height: 70.w,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: StyleHelper.borderRadiusBig,
-      ),
-      child: AppInkWell(
-        padding: EdgeInsets.symmetric(horizontal: 16.w),
-        onPressed: () => CameraDetailsPopUp.openDialog(camera: camera),
-        child: Center(
-          child: Row(
-            children: [
-              SvgPicture.asset(AssetsPathHelper.filledCamera),
-              SizedBox(width: 16.w),
-              Expanded(
-                child: ThemedText(
-                  camera.getName(),
-                  themedTextStyle: ThemedTextStyle.body,
-                  maxLines: 2,
-                ),
-              ),
-              SizedBox(width: 16.w),
-              SvgPicture.asset(AssetsPathHelper.filledDraggable),
-            ],
+    return AppListCard.normal(
+      onPressed: () => CameraDetailsPopUp.openDialog(camera: camera),
+      child: Row(
+        children: [
+          SvgPicture.asset(AssetsPathHelper.filledCamera),
+          SizedBox(width: 16.w),
+          Expanded(
+            child: ThemedText(
+              camera.getName(),
+              themedTextStyle: ThemedTextStyle.body,
+              maxLines: 2,
+            ),
           ),
-        ),
+          SizedBox(width: 16.w),
+          SvgPicture.asset(AssetsPathHelper.filledDraggable),
+        ],
       ),
     );
   }
