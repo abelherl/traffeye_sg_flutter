6c7cdbcf98a9f672b4f6f1720389a060087c4107
abelherl <abangabel02@gmail.com>
2023-09-16 20:05:30 +0700

    [CHG] Updated App List Card implementation and deleted unused file

--- lib/2_application/core/widgets/app_list_card.dart
+++ lib/2_application/core/widgets/app_list_card.dart
@@ -3,15 +3,47 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:traffeye_sg_flutter/1_domain/entities/traffic_camera_entity.dart';
 import 'package:traffeye_sg_flutter/2_application/core/helpers/assets_path_helper.dart';
 import 'package:traffeye_sg_flutter/2_application/core/helpers/style_helper.dart';
 import 'package:traffeye_sg_flutter/2_application/core/widgets/app_ink_well.dart';
+import 'package:traffeye_sg_flutter/2_application/presentation/camera_details/camera_details_pop_up.dart';
 import 'package:traffeye_sg_flutter/2_application/widgets/themed_text.dart';
 import 'package:traffeye_sg_flutter/theme.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 abstract class AppListCard implements Widget {}
 
 // * Base card
+
+class _BaseAppListCard extends StatelessWidget {
+  final Widget child;
+  final Function() onPressed;
+
+  const _BaseAppListCard({
+    required this.child,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(16.w).copyWith(bottom: 0),
+      height: 70.w,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: StyleHelper.borderRadiusBig,
+      ),
+      child: AppInkWell(
+        padding: EdgeInsets.symmetric(horizontal: 16.w),
+        onPressed: onPressed,
+        child: Center(
+          child: child,
+        ),
+      ),
+    );
+  }
+}
 
 class _BaseSmallAppListCard extends StatelessWidget implements AppListCard {
   final Widget child;
@@ -33,6 +65,7 @@ class _BaseSmallAppListCard extends StatelessWidget implements AppListCard {
       ),
       child: AppInkWell(
         padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 12),
+        borderRadius: StyleHelper.borderRadiusSmall,
         onPressed: onPressed,
         child: Center(
           child: child,
@@ -44,21 +77,33 @@ class _BaseSmallAppListCard extends StatelessWidget implements AppListCard {
 
 // * Card variants
 
-class NormalAppListCard extends StatelessWidget implements AppListCard {
-  final Widget child;
-  final Function() onPressed;
+class CameraAppListCard extends StatelessWidget implements AppListCard {
+  final TrafficCameraEntity camera;
 
-  const NormalAppListCard({
+  const CameraAppListCard({
     super.key,
-    required this.child,
-    required this.onPressed,
+    required this.camera,
   });
 
   @override
   Widget build(BuildContext context) {
-    return _BaseSmallAppListCard(
-      onPressed: onPressed,
-      child: child,
+    return _BaseAppListCard(
+      onPressed: () => CameraDetailsPopUp.openDialog(camera: camera),
+      child: Row(
+        children: [
+          SvgPicture.asset(AssetsPathHelper.filledCamera),
+          SizedBox(width: 16.w),
+          Expanded(
+            child: ThemedText(
+              camera.getName(),
+              themedTextStyle: ThemedTextStyle.body,
+              maxLines: 2,
+            ),
+          ),
+          SizedBox(width: 16.w),
+          SvgPicture.asset(AssetsPathHelper.filledDraggable),
+        ],
+      ),
     );
   }
 }
@@ -97,6 +142,40 @@ class ProfileBasicListCard extends StatelessWidget implements AppListCard {
           ),
           SizedBox(width: 10.w),
           SvgPicture.asset(AssetsPathHelper.filledNext),
+        ],
+      ),
+    );
+  }
+}
+
+class ProfileRedirectListCard extends StatelessWidget implements AppListCard {
+  final String title;
+  final String url;
+
+  const ProfileRedirectListCard({
+    super.key,
+    required this.title,
+    required this.url,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return _BaseSmallAppListCard(
+      onPressed: () => launchUrl(
+        Uri.parse(url),
+        mode: LaunchMode.externalApplication,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: ThemedText(
+              title,
+              themedTextStyle: ThemedTextStyle.body,
+              maxLines: 2,
+            ),
+          ),
+          SizedBox(width: 10.w),
+          SvgPicture.asset(AssetsPathHelper.filledRedirect),
         ],
       ),
     );
@@ -179,32 +258,3 @@ class ProfileSelectedListCard extends StatelessWidget {
     );
   }
 }
-
-// class _BaseAppListCard extends StatelessWidget {
-//   final Widget child;
-//   final Function() onPressed;
-//
-//   const _BaseAppListCard({
-//     required this.child,
-//     required this.onPressed,
-//   });
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       margin: EdgeInsets.all(16.w).copyWith(bottom: 0),
-//       height: 70.w,
-//       decoration: BoxDecoration(
-//         color: Colors.white,
-//         borderRadius: StyleHelper.borderRadiusBig,
-//       ),
-//       child: AppInkWell(
-//         padding: EdgeInsets.symmetric(horizontal: 16.w),
-//         onPressed: onPressed,
-//         child: Center(
-//           child: child,
-//         ),
-//       ),
-//     );
-//   }
-// }
--- lib/2_application/presentation/saved_cameras/widgets/saved_cameras_list.dart
+++ lib/2_application/presentation/saved_cameras/widgets/saved_cameras_list.dart
@@ -2,8 +2,8 @@ import 'package:drag_and_drop_lists/drag_and_drop_lists.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:traffeye_sg_flutter/2_application/controllers/saved_cameras_controller.dart';
+import 'package:traffeye_sg_flutter/2_application/core/widgets/app_list_card.dart';
 import 'package:traffeye_sg_flutter/2_application/core/widgets/app_scroll_view.dart';
-import 'package:traffeye_sg_flutter/2_application/presentation/saved_cameras/widgets/saved_camera_list_item.dart';
 
 class SavedCamerasList extends StatelessWidget {
   const SavedCamerasList({Key? key}) : super(key: key);
@@ -22,7 +22,7 @@ class SavedCamerasList extends StatelessWidget {
               canDrag: false,
               children: controller.savedCameras.map((camera) {
                 return DragAndDropItem(
-                  child: SavedCameraListItem(camera: camera),
+                  child: CameraAppListCard(camera: camera),
                 );
               }).toList(),
             ),
