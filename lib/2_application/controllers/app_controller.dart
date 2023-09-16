9f75d7fbbd450fba6fd96a9f6bff95a1e9d18459
abelherl <abangabel02@gmail.com>
2023-09-16 18:37:56 +0700

    [CHG] Small refactor for AppController

--- lib/2_application/controllers/app_controller.dart
+++ lib/2_application/controllers/app_controller.dart
@@ -11,18 +11,10 @@ enum DashboardLayout {
 }
 
 class AppController extends GetxController {
-  final _isSavedCamerasActive = true.obs;
-  final _isExploreCamerasActive = true.obs;
-  final _isHomeWidgetsActive = true.obs;
+  final isSavedCamerasActive = true.obs;
+  final isExploreCamerasActive = true.obs;
+  final isHomeWidgetsActive = true.obs;
   final selectedDashboardLayout = DashboardLayout.carousel.obs;
-
-  bool get isSavedCamerasActive => _isSavedCamerasActive.value;
-  bool get isExploreCamerasActive => _isExploreCamerasActive.value;
-  bool get isHomeWidgetsActive => _isHomeWidgetsActive.value;
-
-  void toggleSavedCameras() => _isSavedCamerasActive.toggle();
-  void toggleExploreCameras() => _isExploreCamerasActive.toggle();
-  void toggleHomeWidgets() => _isHomeWidgetsActive.toggle();
 
   void changeDashboardLayoutTo(DashboardLayout dashboardLayout) {
     selectedDashboardLayout.value = dashboardLayout;
