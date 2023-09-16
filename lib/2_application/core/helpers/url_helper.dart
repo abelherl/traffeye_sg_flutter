48bfe6d099ce874bd39dbc9c30e512617fb1e877
abelherl <abangabel02@gmail.com>
2023-09-16 19:10:38 +0700

    [ADD] Added URL helper

---
+++ lib/2_application/core/helpers/url_helper.dart
@@ -0,0 +1,8 @@
+abstract class UrlHelper {
+  static const flutter = 'https://flutter.dev/';
+  static const iconsax = 'https://iconsax.io/';
+  static const unsplash = 'https://unsplash.com/';
+  static const cameraApi = 'https://beta.data.gov.sg/datasets/354/view';
+  static const traffEyeFigma = 'https://www.figma.com/file/BEvO9y60vPeHFPDyjDMVyZ/TraffEye-SG---Mobile-App?type=design&node-id=95%3A1489&mode=design&t=7Fub4aeMgVtYNPSw-1';
+  static const traffEyeGitHub = 'https://github.com/abelherl/traffeye_sg_flutter';
+}
