359a582e0495904474afb7598b144cea428fbf52
abelherl <abangabel02@gmail.com>
2023-09-16 23:15:27 +0700

    [ADD] Added Keys Helper

---
+++ lib/2_application/core/helpers/keys_helper.dart
@@ -0,0 +1,7 @@
+abstract class KeysHelper {
+  static const isSavedCamerasActive = 'is_saved_cameras_active';
+  static const isExploreCamerasActive = 'is_explore_cameras_active';
+  static const isHomeWidgetsActive = 'is_home_widgets_active';
+  static const lastUpdatedDateTime = 'last_updated_date_time';
+  static const selectedDashboardLayout = 'selected_dashboard_layout';
+}
