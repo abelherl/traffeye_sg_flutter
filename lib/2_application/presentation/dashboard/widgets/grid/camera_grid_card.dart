8dce39a36d84412ad30b0b6e7e957d3378563a03
abelherl <abangabel02@gmail.com>
2023-09-17 09:16:08 +0700

    [ADD] Finished implementing Grid dashboard layout

---
+++ lib/2_application/presentation/dashboard/widgets/grid/camera_grid_card.dart
@@ -0,0 +1,40 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:traffeye_sg_flutter/1_domain/entities/traffic_camera_entity.dart';
+import 'package:traffeye_sg_flutter/2_application/core/helpers/style_helper.dart';
+import 'package:traffeye_sg_flutter/2_application/core/widgets/app_ink_well.dart';
+import 'package:traffeye_sg_flutter/2_application/presentation/camera_details/camera_details_pop_up.dart';
+
+class CameraGridCard extends StatelessWidget {
+  final TrafficCameraEntity camera;
+
+  const CameraGridCard({super.key, required this.camera});
+
+  @override
+  Widget build(BuildContext context) {
+    final borderRadius = StyleHelper.borderRadiusBig;
+
+    return CachedNetworkImage(
+      imageUrl: camera.imageUrl,
+      imageBuilder: (context, image) {
+        return AspectRatio(
+          aspectRatio: StyleHelper.ratioCarousel,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: borderRadius,
+              image: DecorationImage(
+                image: image,
+                fit: BoxFit.fill,
+              ),
+            ),
+            child: AppInkWell(
+              borderRadius: borderRadius,
+              onPressed: () => CameraDetailsPopUp.openDialog(camera: camera),
+              child: Container(),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/2_application/presentation/dashboard/widgets/grid/camera_grid_view.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:traffeye_sg_flutter/2_application/controllers/camera_controller.dart';
+import 'package:traffeye_sg_flutter/2_application/core/helpers/intl_helper.dart';
+import 'package:traffeye_sg_flutter/2_application/core/helpers/style_helper.dart';
+import 'package:traffeye_sg_flutter/2_application/core/widgets/warning_widget.dart';
+import 'package:traffeye_sg_flutter/2_application/presentation/dashboard/widgets/grid/camera_grid_card.dart';
+
+class CameraGridView extends StatelessWidget {
+  const CameraGridView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.find<CameraController>();
+
+    return controller.obx(
+      (state) {
+        return Obx(
+          () => controller.savedCameras.isEmpty
+              ? WarningWidget(
+                  title: IntlHelper.errorNoSavedCamerasTitle.tr,
+                  subtitle: IntlHelper.errorNoSavedCamerasSubtitle.tr,
+                )
+              : _GridView(),
+        );
+      },
+      onLoading: Shimmer.fromColors(
+        baseColor: Colors.grey.shade300,
+        highlightColor: Colors.white,
+        child: _GridView(),
+      ),
+    );
+  }
+}
+
+class _GridView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.find<CameraController>();
+
+    return GridView.builder(
+      shrinkWrap: true,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 18.w,
+        crossAxisSpacing: 18.w,
+        childAspectRatio: StyleHelper.ratioGrid,
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 16.w),
+      itemCount: controller.savedCameras.length,
+      itemBuilder: (_, index) =>
+          CameraGridCard(camera: controller.savedCameras[index]),
+    );
+  }
+}
