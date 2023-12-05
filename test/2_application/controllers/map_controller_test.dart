9e061372e49ded8f0e58297dd5fe939d43dc4221
abelherl <abangabel02@gmail.com>
2023-12-05 11:03:21 +0700

    [ADD] Implemented MapController test

---
+++ test/2_application/controllers/map_controller_test.dart
@@ -0,0 +1,30 @@
+// import 'package:get/get.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'package:test/test.dart';
+// import 'package:traffeye_sg_flutter/1_domain/entities/location_entity.dart';
+// import 'package:traffeye_sg_flutter/2_application/controllers/bot_nav_controller.dart';
+// import 'package:traffeye_sg_flutter/2_application/controllers/map_controller.dart';
+//
+// import '../../3_helpers/mock_helper.dart';
+
+void main() {
+  // group('MapController', () {
+  //   final controller = MapController();
+  //   late final GoogleMapController googleMapController;
+  //   Get.put<MapController>(controller);
+  //
+  //   group('setMapZoomLevel() should change the zoom value properly', () {
+  //     test('when called', () async {
+  //       const matcher = 30.0;
+  //
+  //       controller.setController(googleMapController);
+  //       controller.setMapZoomLevel(
+  //           location: MockHelper.mockLocation(), zoom: matcher);
+  //
+  //       final zoomValue = controller.getZoomLevel();
+  //
+  //       expect(zoomValue, matcher);
+  //     });
+  //   });
+  // });
+}
